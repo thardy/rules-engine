a291ca01f9a7acb81e344e8c7a06a59090e4fe26
Tim Hardy <thardy@signifyhealth.com>
2020-11-17 17:38:04 -0600

    got condition matching working using underscore-query module

--- src/app/engine/rules.service.spec.ts
+++ src/app/engine/rules.service.spec.ts
@@ -4,6 +4,12 @@ import { RulesService } from './rules.service';
 
 describe('RulesService', () => {
   let service: RulesService;
+  const productFacts = [
+    { name: "Widget", quantity: 32, price: 19.99 },
+    { name: "Doohicky", quantity: 900, price: 9.99 },
+    { name: "Thingamajig", quantity: 80, price: 45.95 },
+  ];
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -14,14 +20,43 @@ describe('RulesService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('someFunction', () => {
-    it('should do something good', async () => {
+  describe('isMatch', () => {
+    it('should return the results for conditions that match the facts', async () => {
       // setup
-      
+      const rule = {
+        conditions: {
+          quantity: {$gt:50}
+        }
+      }
+
       // act
+      const results = service.isMatch(productFacts, rule);
 
       // assert
+      expect(results).toBeTruthy();
+      expect(results.length).toEqual(2);
+      expect(results.find((item) => item.name === 'Doohicky')).toBeTruthy();
+      expect(results.find((item) => item.name === 'Thingamajig')).toBeTruthy();
+    });
 
-    })
-  })
+    it('can match conditions when facts contain a single document', async () => {
+      // setup
+      const rule = {
+        conditions: {
+          quantity: {$gt:50}
+        }
+      }
+
+      // act
+      const results = service.isMatch([productFacts[2]], rule);
+
+      // assert
+      expect(results).toBeTruthy();
+      expect(results.length).toEqual(1);
+      expect(results.find((item) => item.name === 'Thingamajig')).toBeTruthy();
+    });
+  });
+
+
+
 });
--- src/app/engine/rules.service.ts
+++ src/app/engine/rules.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash-es';
+import * as underscoreQuery from 'underscore-query';
+const query = underscoreQuery(_, false);
 
 @Injectable({
   providedIn: 'root'
 })
 export class RulesService {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   // facts
   // rules
@@ -13,4 +18,14 @@ export class RulesService {
   //  - cycle through all rules in order
   //  - see if each rule matches the facts (isActive/isMatch?)
   //  - perform actions for rule
+
+  isMatch(facts, rule) {
+    let isMatch = false;
+
+    const queryResult = query(facts, rule.conditions);
+
+    return queryResult;
+  }
+
+
 }
